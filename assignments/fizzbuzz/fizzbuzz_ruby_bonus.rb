80d97c8b3a1f19fcb2059b511af4bdd81125babc
Tim <tim.blonski@gmail.com>
2013-07-26 11:27:53 -0400

    merging

--- assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
+++ assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
@@ -1,1 +1,16 @@
-# Done in JS? Now do it in Ruby. 
+# Done in JS? Now do it in Ruby.
+
+numbers = (1..100).to_a
+
+numbers.each do |number|
+  if number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  elsif number % 15 == 0
+    puts "FizzBuzz"
+  else
+    puts number
+  end
+end
+
--- assignments/fizzbuzz/js/fizzbuzz.js
+++ assignments/fizzbuzz/js/fizzbuzz.js
@@ -1,1 +1,20 @@
-// Your code goes here
+
+var numbers = _.range(1,101);
+
+
+_.map(numbers, function(number) {
+  if (number % 15 == 0){
+    console.log("FizzBuzz");
+  }
+  else if (number % 3 != 0 && number % 5 == 0) {
+    console.log("Buzz");
+  }
+  else if (number % 3 == 0 && number % 5!= 0) {
+    console.log("Fizz");
+  }
+  else {
+    console.log(number);
+  }
+});
+
+
